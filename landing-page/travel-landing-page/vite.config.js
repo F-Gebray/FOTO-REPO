8e5af22ceebfe9f370b63ff3a7b759d62ba5a1b6
Fitwi-Gebray <fitwigebrya@outlook.com>
2026-03-28 18:02:46 +0100

    viteConfig outDir added

--- landing-page/travel-landing-page/vite.config.js
+++ landing-page/travel-landing-page/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist", // ensures Vite outputs to dist folder
+  },
+  root: ".", // optional, defaults to current folder
 });
