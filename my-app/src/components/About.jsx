cea8e7315f9ca72a6ea2c7a513ea4bbc6b194c7d
Fitwi-Gebray <fitwigebrya@outlook.com>
2026-04-01 09:04:53 +0200

    url links modified in about section in my-app

--- my-app/src/components/About.jsx
+++ my-app/src/components/About.jsx
@@ -51,7 +51,7 @@ const About = () => {
                 Demonstrates routing, reusable components, and scalable folder
                 structure.
                 <a
-                  href="https://foto-repo-booking.vercel.app/"
+                  href="https://foto-booking.vercel.app/"
                   target="_blank"
                   rel="noreferrer"
                   className="text-[#3b82f6] hover:text-[#60a5fa] ml-1 transition-colors underline underline-offset-4 decoration-[#3b82f6]/30 font-medium"
@@ -67,7 +67,7 @@ const About = () => {
                 Analytics charts, tables, and dark mode built with React and
                 Recharts.
                 <a
-                  href="https://foto-repo-dashboard-kkzzpd0vb-fitwis-projects.vercel.app/"
+                  href="https://dashboard-d7c57e5f9-fitwis-projects.vercel.app/"
                   target="_blank"
                   rel="noreferrer"
                   className="text-[#3b82f6] hover:text-[#60a5fa] ml-1 transition-colors underline underline-offset-4 decoration-[#3b82f6]/30 font-medium"
@@ -83,7 +83,7 @@ const About = () => {
                 Full-screen UI, destination search, and responsive design built
                 with React.
                 <a
-                  href="https://foto-repo-landing-page-i3z4nhown-fitwis-projects.vercel.app/"
+                  href="https://landing-page-omega-two-35.vercel.app/"
                   target="_blank"
                   rel="noreferrer"
                   className="text-[#3b82f6] hover:text-[#60a5fa] ml-1 transition-colors underline underline-offset-4 decoration-[#3b82f6]/30 font-medium"
